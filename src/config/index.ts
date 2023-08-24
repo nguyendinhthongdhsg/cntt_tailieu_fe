8863c92dfdeb61eaee8de1b387ec54a1b83af426
nguyendinhthongdhsg <nguyendinhthongdhsg@gmail.com>
2023-08-25 01:51:25 +0700

    change be

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,2 +1,2 @@
 // export const URL_BACKEND = 'http://localhost:8000';
-export const URL_BACKEND = 'https://cntt-tailieu-be-2023.vercel.app';
+export const URL_BACKEND = 'https://cntt-tailieu-be-2023-crn6u8jwu-ndthong.vercel.app';
